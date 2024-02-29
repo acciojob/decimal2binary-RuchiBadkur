45504f46355b9a9e0dd5ea7cc1d08319fefaf854
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-02-29 17:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
 function decimalToBinary(num) {
   //Write you code here
-  
+  if(num === 0 || num === 1){
+	  return num;
+  } else{
+	decimalToBinary(Math.floor(num/2)) + (num%2);
+	}
 }
 
 window.decimalToBinary = decimalToBinary;
